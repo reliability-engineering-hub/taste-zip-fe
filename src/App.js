e035bdb8ef0b3c8d362b612978e483de5ace1100
yongckim <zxcv9203@naver.com>
2023-08-13 01:19:00 +0900

    :sparkles: 메인 페이지 라우터 등록

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import Diaries from "./page/Diaries";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path='/' element={<Diaries/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/page/Diaries.js
@@ -0,0 +1,7 @@
+const Diaries = () => {
+    return <div>
+        <h2>맛집 다이어리 메인 페이지</h2>
+    </div>
+}
+
+export default Diaries;
