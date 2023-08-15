b9500978f0909275f0de8eaa5df3b54cfcaa9fe5
yongckim <zxcv9203@naver.com>
2023-08-15 16:06:27 +0900

    :sparkles: 맛집 일기 백엔드 API 요청하여 저장 하는 기능 추가

--- src/component/NewDiary.js
+++ src/component/NewDiary.js
@@ -1,93 +1,119 @@
-import {useCallback, useRef, useState} from "react";
+import {useCallback, useState} from "react";
 import Items from "./Items";
 import {evaluations} from "../util/Evaluation";
 import CustomButton from "./CustomButton";
 import {meals} from "../util/Meal";
 
-const NewDiary = () => {
-    const contentRef = useRef();
 
-    const [meal, setMeal] = useState(false);
+const NewDiary = ({onDataUpdate}) => {
 
+    const [meal, setMeal] = useState(1);
     const [evaluation, setEvaluation] = useState(3);
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+    const [newDiary, setNewDairy] = useState({
+        title: '',
+        eatDate: '',
+        restaurant : {
+            name: '',
+            address: '',
+            image: '',
+        },
+        content: '',
+        evaluation: '',
+    })
 
-    const handleClickMeal = useCallback((meal) => {
-        setMeal(meal);
-    }, [])
-    const handleClickEvaluation = useCallback((evaluation) => {
-        setEvaluation(evaluation);
-    }, [])
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const diary = {}
+    const handleClickMeal = (mealId) => {
+        setMeal(mealId);
+        const updatedDiary = {...newDiary, evaluation: mealId};
+        setNewDairy(updatedDiary)
+        onDataUpdate(updatedDiary)
     }
+
+    const handleClickEvaluation = (evaluationId) => {
+        setEvaluation(evaluationId);
+        const updatedDiary = {...newDiary, evaluation: evaluationId};
+
+        setNewDairy(updatedDiary)
+        onDataUpdate(updatedDiary)
+    };
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        const updatedDiary = {...newDiary, [name]: value};
+        console.log(updatedDiary)
+        setNewDairy(updatedDiary);
+
+        onDataUpdate(updatedDiary)
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="DiaryEditor">
-                <section>
-                    <input
-                        className="DiaryTitle"
-                        type="text"
-                        placeholder="제목을 입력해주세요"
-                    />
-                </section>
-                <section className="MealDate">
-                    <input
-                        className="DiaryDate"
-                        type="date"
-                    />
-                    <div className="input_box meal_box">
-                        {
-                            meals.map((it) =>
-                                <Items
-                                    type={"Meal"}
-                                    key={it.id}
-                                    {...it}
-                                    onClick={handleClickMeal}
-                                    isSelected={it.id === meal}/>
-                            )}
-                    </div>
-                </section>
-                <section className="FindRestaurant">
-                    <input
-                        className="RestaurantName"
-                        type="text"
-                        placeholder="가게 이름을 입력해주세요"
-                    />
-                    <div className="FindRestaurantButton">
-                        <CustomButton
-                            text="🔍"
-                            type="find"
-                            onClick={() => {
-                            }}
-                        />
-                    </div>
-                </section>
-                <textarea
-                    className="DiaryContent"
-                    ref={contentRef}
-                    placeholder="내용을 입력해주세요."
+        <div className="DiaryEditor">
+            <section>
+                <input
+                    className="DiaryTitle"
+                    type="text"
+                    placeholder="제목을 입력해주세요"
+                    name="title"
+                    value={newDiary.title}
+                    onChange={handleChange}
                 />
-                <section>
-                    <h4>방문 후기는 어떠셨나요?</h4>
-                    <div className="input_box evaluation_list_wrapper">
-                        {
-                            evaluations.map((it) =>
-                                <Items
-                                    type={"Evaluation"}
-                                    key={it.id}
-                                    {...it}
-                                    onClick={handleClickEvaluation}
-                                    isSelected={it.id === evaluation}/>
-                            )}
-                    </div>
-                </section>
-            </div>
-        </form>
+            </section>
+            <section className="MealDate">
+                <input
+                    className="DiaryDate"
+                    type="date"
+                    name="eatDate"
+                    value={newDiary.eatDate}
+                    onChange={handleChange}
+                />
+                <div className="input_box meal_box">
+                    {
+                        meals.map((it) =>
+                            <Items
+                                type={"Meal"}
+                                key={it.id}
+                                {...it}
+                                onClick={handleClickMeal}
+                                isSelected={it.id === meal}/>
+                        )}
+                </div>
+            </section>
+            <section className="FindRestaurant">
+                <input
+                    className="RestaurantName"
+                    type="text"
+                    placeholder="가게 이름을 입력해주세요"
+                />
+                <div className="FindRestaurantButton">
+                    <CustomButton
+                        text="🔍"
+                        type="find"
+                        onClick={() => {
+                        }}
+                    />
+                </div>
+            </section>
+            <textarea
+                className="DiaryContent"
+                placeholder="내용을 입력해주세요."
+                name="content"
+                value={newDiary.content}
+                onChange={handleChange}
+            />
+            <section>
+                <h4>방문 후기는 어떠셨나요?</h4>
+                <div className="input_box evaluation_list_wrapper">
+                    {
+                        evaluations.map((it) =>
+                            <Items
+                                type={"Evaluation"}
+                                key={it.id}
+                                {...it}
+                                onClick={handleClickEvaluation}
+                                isSelected={it.id === evaluation}/>
+                        )}
+                </div>
+            </section>
+        </div>
     )
 
 }
--- src/page/New.js
+++ src/page/New.js
@@ -2,27 +2,64 @@ import Header from "../component/Header";
 import NewDiary from "../component/NewDiary";
 import {useNavigate} from "react-router-dom";
 import CustomButton from "../component/CustomButton";
+import {useState} from "react";
+import axios from "axios";
+
 
 const New = () => {
     const navigate = useNavigate();
+
+    const [diary, setDiary] = useState({
+        title: '',
+        eatDate: '',
+        restaurant: {
+            name: '',
+            address: '',
+            image: '',
+        },
+        content: '',
+        evaluation: '',
+    })
+
+    const handleNewDiary = (data) => {
+        setDiary(data);
+    }
+
+    // TODO validation 추가 필요
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const endpoint = process.env.REACT_APP_BACKEND_ENDPOINT + "/diaries";
+
+        try {
+            const response = await axios.post(endpoint, diary)
+            console.log(response.status)
+            if (response.status === 201) {
+                console.log("일기 저장 성공")
+                navigate('/', {replace: true});
+            }
+        } catch (error) {
+            console.error("저장 실패 : ", error);
+        }
+    }
 
     return (
         <div>
             <Header
                 headText={"taste.zip"}
                 leftChild={
-                <CustomButton
-                    text={"<"}
-                    type={"back"}
-                    onClick={() => navigate(-1)}
-                />}
+                    <CustomButton
+                        text={"<"}
+                        type={"back"}
+                        onClick={() => navigate(-1)}
+                    />}
                 rightChild={
-                <CustomButton
+                    <CustomButton
                         text={"저장"}
                         type={"save"}
-                />}
+                        onClick={handleSubmit}
+                    />}
             />
-            <NewDiary />
+            <NewDiary onDataUpdate={handleNewDiary}/>
         </div>
     );
 }
